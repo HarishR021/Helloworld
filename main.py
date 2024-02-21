bd80f74f8489536e6ef13112ce39002949ccde1a
Harish R <hari20ec036@rmkcet.ac.in>
2024-02-21 12:02:44 +0530

    commit message

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+def swapList(newone):
+    size=len(newone)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    temp=newone[0]
+    # 1 3 4 5 6 (5-1=4)
+    newone[0]=newone[size-1]
+    newone[size-1]=temp
+    print("Here is the ouput after swapping")
+    return newone
+newone=[1,3,4,5,6]
+print(swapList(newone))
