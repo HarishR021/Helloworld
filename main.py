2d8894ed860cfe3a08a94162fda389f064f0f9f3
Harish R <hari20ec036@rmkcet.ac.in>
2024-02-21 12:12:21 +0530

    In swapList line 9 added

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def swapList(newone):
     newone[0]=newone[size-1]
     newone[size-1]=temp
     print("Here is the ouput after swapping")
+    print("please check below")
     return newone
 newone=[1,3,4,5,6]
 print(swapList(newone))
