d1cae56672bd7e80f5e748474968a3d904618cc2
Harish R <hari20ec036@rmkcet.ac.in>
2024-02-22 17:00:26 +0530

    added code in line 22&23

--- main.py
+++ main.py
@@ -18,3 +18,35 @@ if n >= 100:
     print(True)
 else:
     print(False)
+
+#if-else branch
+if the_weather_is_good:
+    go_for_a_walk()
+    have_fun()
+else:
+    go_to_a_theater()
+    enjoy_the_movie()
+have_lunch()
+
+#nested if-else statements
+if the_weather_is_good:
+    if nice_restaurant_is_found:
+        have_lunch()
+    else:
+        eat_a_sandwich()
+else:
+    if tickets_are_available:
+        go_to_the_theater()
+    else:
+        go_shopping()
+
+#elif statement
+if the_weather_is_good:
+    go_for_a_walk()
+elif tickets_are_available:
+    go_to_the_theater()
+elif table_is_available:
+    go_for_lunch()
+else:
+    play_chess_at_home()
+
