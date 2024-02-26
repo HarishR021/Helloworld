f1f1f2a188bf879c0b986e6b40dbd1e4e3d9dedf
Harish R <hari20ec036@rmkcet.ac.in>
2024-02-26 11:27:15 +0530

    updated code lins

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Swaplist
+#1 Swaplist
 def swapList(newone):
     size=len(newone)
 
@@ -12,68 +12,8 @@ def swapList(newone):
 newone=[1,3,4,5,6]
 print(swapList(newone))
 
-#test
-n = int(input("enter:"))
-if n >= 100:
-    print(True)
-else:
-    print(False)
 
-#if-else branch
-if the_weather_is_good:
-    go_for_a_walk()
-    have_fun()
-else:
-    go_to_a_theater()
-    enjoy_the_movie()
-have_lunch()
-
-#nested if-else statements
-if the_weather_is_good:
-    if nice_restaurant_is_found:
-        have_lunch()
-    else:
-        eat_a_sandwich()
-else:
-    if tickets_are_available:
-        go_to_the_theater()
-    else:
-        go_shopping()
-
-#elif statement
-if the_weather_is_good:
-    go_for_a_walk()
-elif tickets_are_available:
-    go_to_the_theater()
-elif table_is_available:
-    go_for_lunch()
-else:
-    play_chess_at_home()
-
-#largest among three numbers 3 diff methods
-#1
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-if number1 > number2:
-    larger_number = number1
-else:
-    larger_number = number2
-print("The larger number is:", larger_number)
-
-#2
-# Read three numbers
-number1 = int(input("Enter the first number: "))
-number2 = int(input("Enter the second number: "))
-number3 = int(input("Enter the third number: "))
-# We temporarily assume that the first number is the largest one. We will verify this soon.
-largest_number = number1
-if number2 > largest_number:
-    largest_number = number2
-if number3 > largest_number:
-    largest_number = number3
-print("The largest number is:", largest_number)
-
-#3
+#2 largest among three numbers
 number1 = int(input("Enter the first number: "))
 number2 = int(input("Enter the second number: "))
 number3 = int(input("Enter the third number: "))
@@ -81,7 +21,7 @@ largest_number = max(number1, number2, number3)
 print("The largest number is:", largest_number)
 
 
-#Spathiphyllum plant sampletest
+#3 Spathiphyllum plant sampletest
 name=input("enter flower name:")
 if name=="Spathiphyllum":
     print("Yes - Spathiphyllum is the best plant ever!")
@@ -89,3 +29,5 @@ elif name=="spathiphyllum":
     print("No, I want a big Spathiphyllum!")
 else:
     print("Spathiphyllum! Not",name+"!")
+
+#4
